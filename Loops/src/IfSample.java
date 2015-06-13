d93dbc5f0da8be16707bfe467b725174fe4db5dd
Ranjani <Ranjani@192.168.0.109>
2015-06-13 18:09:19 -0400

    update loops

---
+++ Loops/src/IfSample.java
@@ -0,0 +1,13 @@
+
+public class IfSample{
+	
+	public static void main(String args[]){
+	int x=10;int y=20;
+	x=x*2;
+	if(x==y) System.out.println("x is now equl to y" + " " + x);
+	//x=x*2;
+	//if(x>y) System.out.println("x is now greater than y" + x);
+	
+	}
+	
+	}
